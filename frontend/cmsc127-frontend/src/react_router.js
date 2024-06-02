059dda68bc69ffec6cedd52ec6e19d8f61da2cd8
Erjoy Robles <elrobles1@up.edu.ph>
2024-06-03 00:24:35 +0800

    feat: added search by establishment

--- frontend/cmsc127-frontend/src/react_router.js
+++ frontend/cmsc127-frontend/src/react_router.js
@@ -8,7 +8,8 @@ import FoodByEstablishment from './pages/establishment_list/page_establishment_f
 import FoodReview from './pages/reviews/page_food_review';
 import EstablishmentReview from './pages/reviews/page_establishment_reviews';
 import CreateEstablishment from './pages/establishment_list/page_create_establishment';
-import CreateEstablishmentReview from './pages/reviews/create_establishment_review';
+import SearchEstablishment from './pages/establishment_list/page_search_establishment';
+// import CreateEstablishmentReview from './pages/reviews/create_establishment_review';
 
 function Router() {
 
@@ -22,7 +23,8 @@ function Router() {
       <Route path="/food-review/:food_id" element={<FoodReview />}></Route>
       <Route path="/establishment-review/:establishment_id" element={<EstablishmentReview />}></Route>
       <Route path="/create-establishment" element={<CreateEstablishment />}></Route>
-      <Route path="/create-establishment-review/:establishment_id" element={<CreateEstablishmentReview />}></Route>
+      {/* <Route path="/create-establishment-review/:establishment_id" element={<CreateEstablishmentReview />}></Route> */}
+        <Route path="/search-establishment" element={<SearchEstablishment/>}></Route>
     </Routes>
   )
 }
