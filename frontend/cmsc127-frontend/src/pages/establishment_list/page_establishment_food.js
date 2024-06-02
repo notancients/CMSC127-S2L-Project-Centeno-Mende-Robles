b568a2b76d2c8b917ba8e8d93000214a1a690ce3
Erjoy Robles <elrobles1@up.edu.ph>
2024-06-02 21:00:38 +0800

    feat: now filters food in establishment by category

--- frontend/cmsc127-frontend/src/pages/establishment_list/page_establishment_food.js
+++ frontend/cmsc127-frontend/src/pages/establishment_list/page_establishment_food.js
@@ -7,12 +7,35 @@ import FoodTable from '../components/food_table';
 let SERVER = ENV.SERVER;
 let HEADER = ENV.HEADER;
 
+const categories = [ "Filipino","Greek","Western","Asian","Japanese","Turkish","Vietnamese","Chinese","Fusion","Thai","Russian","Korean" ]
 
 function FoodByEstablishment() {
     // Get a specific query parameter
     const {establishment_id} = useParams();
 
+    let [allFood, setAllFood] = useState([]); // this is used to store the complete array of food served by the restaurant
     let [food, setFood] = useState([]);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+
+    function filterCategory(category) {
+        let foodListByCategory = [];
+        allFood.map((element) => {
+            if(element.category.includes(category)) {
+                console.log(element);
+                foodListByCategory.push(element);
+            }
+        })
+        return foodListByCategory;
+    }
+    const handleClick = (option) => {
+        setFood(filterCategory(option));
+        setIsOpen(false);
+    };
+
 
     useEffect( () => {
         console.log("Fetching food by establishment name.");
@@ -22,7 +45,8 @@ function FoodByEstablishment() {
                     `http://${SERVER}/api/get-food-by-establishment?establishment_id=${establishment_id}`,
                     HEADER
                 );
-                console.log(response.data.data);
+                // console.log(response.data.data);
+                setAllFood(response.data.data)
                 setFood(response.data.data);
             } catch (err) {
                 console.log(err);
@@ -32,10 +56,31 @@ function FoodByEstablishment() {
         fetchFoodByEstablishment();
     }, []);
 
+    function CategoryDropdown() {
+        return(
+        <div className="dropdown">
+            <button onClick={toggleDropdown}>Select by Category</button>
+            {isOpen && (
+            <ul className="dropdown-menu">
+                {categories.map((option) => (
+                <li key={`category_${option}`} onClick={() => handleClick(option)}>
+                    {option}
+                </li>
+                ))}
+            </ul>
+            )}
+        </div>
+        )
+    }
+
     return(
     <>
     <div className='container'>
         <FoodTable data={food}/>
+        <div>
+            <button>Sort by Price</button>
+            <CategoryDropdown/>
+        </div>
     </div>
     </>
     )
