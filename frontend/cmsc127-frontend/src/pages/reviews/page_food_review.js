89df498f89d71cfb9e7c833a72921893bd233cb6
Erjoy Robles <elrobles1@up.edu.ph>
2024-06-02 23:17:50 +0800

    feat: added get review by month for food reviews

--- frontend/cmsc127-frontend/src/pages/reviews/page_food_review.js
+++ frontend/cmsc127-frontend/src/pages/reviews/page_food_review.js
@@ -7,12 +7,17 @@ import ENV from '../../env.js';
 
 let {SERVER, HEADER} = ENV;
 
+let review_range = ["None", "Month"];
+
 function FoodReview({data}) {
     let headers = ['ID', 'Rating', 'Post Content', 'Date Published'];
 
     const {food_id} = useParams();
 
     const [foodReview, setFoodReview] = useState([]);
+
+    const [allFoodReview, setAllFoodReview] = useState([]);
+    const [isOpen, setIsOpen] = useState(false);
 
     useEffect( () => {
         console.log("Fetching food review data.");
@@ -21,7 +26,8 @@ function FoodReview({data}) {
                 const response = await axios.get(
                     `http://${SERVER}/api/view-reviews?target_id=${parseInt(food_id)}&review_type=0`,
                 );
-                console.log(response);
+                // console.log(response);
+                setAllFoodReview(response.data.data);
                 setFoodReview(response.data.data);
             } catch (err) {
                 console.log(err);
@@ -31,9 +37,55 @@ function FoodReview({data}) {
         fetchReviews();
     }, []);
 
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    function handleReviewRange(timeFilter) {
+        let newReviewArray = [];
+
+        
+        if (timeFilter === "None") {
+            newReviewArray = allFoodReview;
+        } else {
+            const millisecondsPerDay = 1000 * 60 * 60 * 24;
+            const thirtyDaysAgo = (new Date()).getTime() - (millisecondsPerDay * 30);
+
+            allFoodReview.map( (element) => {
+                let date = element.date_published.substring(0, 10);
+                date = date.split('-');
+                date = new Date(parseInt(date[0]), parseInt(date[1])-1, parseInt(date[2]))
+                
+                if(new Date(thirtyDaysAgo)-0 <= new Date(date)-0) {
+                    
+                    newReviewArray.push(element);
+                }
+            } )
+        }
+
+        setAllFoodReview(newReviewArray);
+        setIsOpen(false);
+    }
+
+    function ReviewRangeDropdown() {
+        return(
+        <div className="dropdown">
+            <button onClick={toggleDropdown}>Select by time range</button>
+            {isOpen && (
+            <ul className="dropdown-menu">
+                {review_range.map((option) => (
+                <li key={`range_${option}`} onClick={() => handleReviewRange(option)}>
+                    {option}
+                </li>
+                ))}
+            </ul>
+            )}
+        </div>
+        )
+    }
+
     return(
     <>
     <div className="container">
+    <ReviewRangeDropdown/>
     <table>
         <thead>
             <tr>
